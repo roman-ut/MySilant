a2d5e56522751ceb82aa186a209b2f3d98afa8f6
R.Utochkin <utochkin.re@gmail.com>
2023-01-26 02:31:02 +0300

    #3_

--- models/ModelMachine.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type ModelMachine = {
-    readonly id?: number;
-    title: string;
-    description: string;
-};
--- models/RecoveryMethod.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type RecoveryMethod = {
-    readonly id?: number;
-    title: string;
-    description: string;
-};
