d92dd09258eee5ce623002b9b24cf94e16e0f4ab
R.Utochkin <utochkin.re@gmail.com>
2023-01-27 00:14:12 +0300

    #5

--- models/ModelTransmission.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type ModelTransmission = {
-    readonly id?: number;
-    title: string;
-    description: string;
-};
--- models/TypeFailure.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type TypeFailure = {
-    readonly id?: number;
-    title: string;
-    description: string;
-};
